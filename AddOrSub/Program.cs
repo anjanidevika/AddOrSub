21beaa5cafe12d7551786b4d088a1d60297e18b4
anjanidevika <adevika@kiproshamerica.com>
2023-04-27 09:27:38 +0530

    Add project files.

---
+++ AddOrSub/Program.cs
@@ -0,0 +1,20 @@
+﻿//program that adds when a<b and subtracts when a>b
+using System;
+public class Program
+{
+    static void addorsub(int a, int b)
+    {
+        if (a > b)
+            Console.WriteLine("the numbers are subtracted:" + (a - b));
+        else
+            Console.WriteLine("the numbers are added:" + (a + b));
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter another number");
+        int b = Convert.ToInt32(Console.ReadLine());
+        addorsub(a, b);
+    }
+}
